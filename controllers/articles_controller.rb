eb82dc6d868aa2f174937e2c5cb19b3d649c0a07
Unknown <selimchiki@yahoo.fr>
2018-12-18 20:45:53 +0100

    Create basic generator for CRUD controller

---
+++ controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+Class ArticlesController < BaseController
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- lib/generators/controller.rb
+++ lib/generators/controller.rb
@@ -1,7 +1,52 @@
 module Generators
   class Controller
     def self.generate(name:)
-      puts "Hello, #{name}"
+      puts new(name).generate
+    end
+
+    def initialize(name)
+      @filename = "#{name}_controller.rb"
+      @classname = name.split('_').map(&:capitalize).join
+      @name = name.capitalize
+    end
+
+    def generate
+      File.write("#{path}/#{filename}", code)
+    end
+
+    private
+
+    attr_reader :filename
+
+    def path
+      "controllers"
+    end
+
+    def code
+      <<~CODE
+        Class #{@classname}Controller < BaseController
+          def index
+          end
+
+          def show
+          end
+  
+          def delete
+          end
+
+          def new
+          end
+
+          def create
+          end
+
+          def edit
+          end
+
+          def update
+          end
+        end
+      CODE
     end
   end
 end
