82fc113d6561009408368eabe747d99bd1851898
Unknown <selimchiki@yahoo.fr>
2018-12-18 18:02:12 +0100

    Initial Repo for add API feature

--- db/migrations/02_add_gif_to_posts.rb
+++
@@ -1,5 +0,0 @@
-Sequel.migration do
-  change do
-    add_column :posts, :gif, String, :text => true
-  end
-end
--- routes.rb
+++ routes.rb
@@ -1,11 +1,3 @@
 Application.routes.config do
-
-  get "/posts", route_to: "posts#index"
-  get "/posts/new", route_to: "posts#new"
-  get "/posts/show", route_to: "posts#show"
-  get "/posts/delete", route_to: "posts#delete"
-  post "/posts/new", route_to: "posts#create"
-  get "/posts/edit", route_to: "posts#edit"
-  post "/posts/edit", route_to: "posts#update"
-
+  
 end
